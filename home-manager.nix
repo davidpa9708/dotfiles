1f874cfa3ca9562c2f833ffa673998495a920fe7
David Perez Alvarez <david@leddgroup.com>
2024-01-09 20:42:30 +0000

    basic config

--- home-manager.nix
+++ home-manager.nix
@@ -1,9 +1,83 @@
 { config, pkgs, ... }:
 {
-  home.username = "david";
-  home.homeDirectory = "/home/david";
+  
+  home = {
+    username = "david";
+    homeDirectory = "/home/david";
+    stateVersion = "23.11";
+    
+    packages = with pkgs; [
+      
+      emacs29
+      git
+      micro
+      signal-desktop
+      firefox
+      keepassxc
+      logseq
+      starship
+      # android-tools
+      
+    ];
+     
+    shellAliases = {
+      ne = "sudo -E emacs /etc/nixos/configuration.nix";
+      nr = "sudo nixos-rebuild switch";     
+    };
+    
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    
+    
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
+        
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
+    
+    sessionVariables = {
+      # EDITOR = "emacs";
+    };
+  };
+  
+  programs = {
+    fish.enable = true;
+    starship.enable = true;
+    starship.enableFishIntegration = true;
+    git = {
+      enable = true;
+      userName = "David Perez Alvarez";
+      userEmail = "david@leddgroup.com";
+    };
+    #xsession.enable = true;
+    home-manager.enable = true;  
+  };
 
-  home.stateVersion = "23.11";
+  
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/david/etc/profile.d/hm-session-vars.sh
+  #
 
-  programs.home-manager.enable = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+
 }
