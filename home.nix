568f38c851c5a240883589971638f484053e346b
David Perez Alvarez <david@leddgroup.com>
2025-05-15 22:31:19 -0700

    nix changes

--- home.nix
+++ home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, pkgs-unstable, lib, buildPythonPackage, ... }:
 let
-  pkgs-unstable = import <nixpkgs-unstable> { };
-  pkgs-23-11 = import <nixos-23.11> { };
+  pkgs-unstable = import <home-manager> { };
+  pkgs-23-11 = import <home-manager> { };
 in {
   home = {
     username = "david";
@@ -11,89 +11,69 @@ in {
     packages = (with pkgs; [
       ## essentials
       starship
-      git
+      #git
       # syncthing
-      ranger
+      #ranger
 
       ## editors
-      emacs29
-      micro
+      # emacs-29
+	  typescript-language-server
+      #micro
 
       ## work
-      vscodium
-      slack
-      mongodb-compass
+      #vscodium
+      #slack
+      #mongodb-compass
 
       ## apps
-      signal-desktop
-      firefox
-      libreoffice-qt
-      qutebrowser
-      keepassxc
-      logseq
-      thunderbird
-      ungoogled-chromium
-      vlc
-      freetube
-      spotify
+      #signal-desktop
+      #firefox
+      #libreoffice-qt
+      #qutebrowser
+      #keepassxc
+      #logseq
+      #thunderbird
+      #ungoogled-chromium
+      #vlc
+      #freetube
+      #spotify
       # feishin # spotify client
 
-      tome4 # game
+      #tome4 # game
 
       ## creating media:
-      inkscape
-      blender
-      gimp
-      krita
-      obs-studio
+      #inkscape
+      #blender
+      #gimp
+      #krita
+      #obs-studio
 
       ## others
       # libsForQt5.polonium
-      nixfmt-classic
-      protonvpn-cli_2
+      #nixfmt-classic
+      #protonvpn-cli_2
       fd
       ripgrep
-      zeal
-      wakatime
+      #zeal
+      #wakatime
+	  kanata
 
       ## hyprland
-      libnotify
-      kitty
-      swaynotificationcenter
-      kdePackages.polkit-kde-agent-1
-      pipewire
-      wireplumber
-      playerctl
-      tofi # wofi
+      #libnotify
+      #kitty
+      #swaynotificationcenter
+      #kdePackages.polkit-kde-agent-1
+      #pipewire
+      #wireplumber
+      #playerctl
+      #tofi # wofi
       # waybar
       # hyprpaper
-      eww
-      wmctrl
-      networkmanagerapplet
+      #eww
+      #wmctrl
+      #networkmanagerapplet
       flameshot
-    ]) ++ (with pkgs-unstable;
-      [
-        proton-pass # protonmail-desktop
-      ]) ++ (with pkgs-23-11; [ standardnotes ])
-      ++ (with pkgs-unstable.nodePackages_latest;
-        [
-          npm-check-updates
-          prettier
-          typescript
-          typescript-language-server
-          vscode-langservers-extracted
-          yaml-language-server
-          bash-language-server
-          dockerfile-language-server-nodejs
-        ]
-
-        ++ (with pkgs-unstable; [
-          ## godot stuff:
-          gdtoolkit_4
-          godot_4
-        ])
-
-      );
+    ]);
 
     shellAliases = {
       ne = "sudo -E emacs ~/dotfiles/configuration.nix";
@@ -119,13 +99,13 @@ in {
       # };
     };
 
-    sessionVariables = { EDITOR = "emacs"; };
+    sessionVariables = { EDITOR = "emacsclient -t"; };
 
     activation = {
       myActivationAction = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         $DRY_RUN_CMD ln -fs $VERBOSE_ARG ${
           builtins.toPath ./.
-        } $HOME/.config/home-manager/
+        } $HOME/.config/home-manager
         $DRY_RUN_CMD ln -fs $VERBOSE_ARG ${
           builtins.toPath ./emacs.d
         } $HOME/.emacs.d
@@ -137,7 +117,6 @@ in {
         } $HOME/.config/eww
       '';
     };
-
   };
 
   programs = {
@@ -168,55 +147,39 @@ in {
       #enableBashIntegration = true; # see note on other shells below
       #enableFishIntegration = true;
     };
-    librewolf = {
-      enable = true;
-      settings = {
-        "privacy.resistFingerprinting.letterboxing" = true;
-        "webgl.disabled" = false;
-        "privacy.clearOnShutdown.history" = false;
-        "privacy.clearOnShutdown.downloads" = false;
-        "identity.fxaccounts.enabled" = true;
-        "middlemouse.paste" = false;
-      };
-    };
+    ## librewolf = {
+    ##   enable = true;
+    ##   settings = {
+    ##     "privacy.resistFingerprinting.letterboxing" = true;
+    ##     "webgl.disabled" = false;
+    ##     "privacy.clearOnShutdown.history" = false;
+    ##     "privacy.clearOnShutdown.downloads" = false;
+    ##     "identity.fxaccounts.enabled" = true;
+    ##     "middlemouse.paste" = false;
+    ##   };
+    ## };
   };
 
-  nixpkgs.config.permittedInsecurePackages =
-    [ "electron-25.9.0" "electron-24.8.6" "electron-27.3.11" ]; # logseq feishin
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "slack"
-      "mongodb-compass"
-      "spotify"
-      "keymapp"
-    ];
-
-  # https://wiki.hyprland.org/Nix/Hyprland-on-Home-Manager/
-  home.sessionVariables.NIXOS_OZONE_WL = "1";
-  # https://wiki.hyprland.org/Nix/Hyprland-on-Home-Manager/
-  home.pointerCursor = {
-    gtk.enable = true;
-    # x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 16;
-  };
-  gtk = {
+  systemd.user = {
     enable = true;
 
-    theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
-    };
-
-    iconTheme = {
-      package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-
-    font = {
-      name = "Sans";
-      size = 11;
-    };
+	services = {
+      # https://github.com/jtroo/kanata/discussions/130#discussioncomment-10227272
+	  kanata = {
+	    Unit = {
+		  Description = "My NIX Kanata Service";
+		  Requires="local-fs.target";
+		  After="local-fs.target";
+		};
+        Service = {
+		  ExecStartPre="/usr/sbin/modprobe uinput";
+		  ExecStart="kanata -c /etc/kanata/kanata.kbd";
+          Restart="no";
+        };
+        Install = {
+		  WantedBy = [ "sysinit.target" ];
+		};
+	  };
+	};
   };
 }
