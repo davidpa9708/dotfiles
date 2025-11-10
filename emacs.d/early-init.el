3ed2f07ff3d4da200fa26069e031a3f053d2ff31
David Perez Alvarez <david@leddgroup.com>
2025-11-09 23:33:10 -0800

    [emacs] comment early-init.el term related

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,16 +1,16 @@
-(defun start-term ()
-  (if (string-equal (buffer-name (current-buffer)) "*scratch*")
-	  (progn
-		(term "/bin/bash")
-		;; (term-line-mode)
-		(rename-uniquely)
-		)))
+;; (defun start-term ()
+;;   (if (string-equal (buffer-name (current-buffer)) "*scratch*")
+;; 	  (progn
+;; 		(term "/bin/bash")
+;; 		;; (term-line-mode)
+;; 		(rename-uniquely)
+;; 		)))
 
-(add-hook 'after-make-frame-functions
-		  (lambda (frame)
-			(select-frame frame)
-			(start-term)))
+;; (add-hook 'after-make-frame-functions
+;; 		  (lambda (frame)
+;; 			(select-frame frame)
+;; 			(start-term)))
 
 
-(add-hook 'server-after-make-frame-hook 'start-term)
+;; (add-hook 'server-after-make-frame-hook 'start-term)
 
